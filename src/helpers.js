55819e6956085f6ca51b6d82173fd65e1c094680
Nicolas Udy <dev@udy.io>
2019-04-19 02:50:53 -0400

    Update tests

--- src/helpers.js
+++ src/helpers.js
@@ -1,19 +1,20 @@
+export const isFunction = value => typeof value === 'function'
+export const isNumber = value => typeof value === 'number' && !isNaN(value)
+export const isObject = value => value !== null && !Array.isArray(value) && typeof value === 'object'
+export const isNullOrUndefined = (value = null) => value === null
+
 export const isRegExp = value => {
   const safeValue = value || ''
   return safeValue.constructor.name === RegExp.name
 }
 
-export const isFunction = value => typeof value === 'function'
-
 export const isEmpty = (value = null) => {
-  if (value === null || value === undefined) return true
+  if (isNullOrUndefined(value)) return true
 
-  const isBoolean = typeof value === 'boolean'
-  const isNumber = typeof value === 'number'
-  const isObject = !Array.isArray(value) && typeof value === 'object'
-  const isImpliedValue = isBoolean || isNumber || isFunction(value)
-  const isEmptyObject = isObject && !isRegExp(value) && Object.keys(value).length === 0
-  return isEmptyObject || (!isImpliedValue && !isObject && !value.length)
+  const isValueObject = isObject(value)
+  const isImpliedValue = isNumber(value) || isFunction(value) || value === true
+  const isEmptyObject = isValueObject && !isRegExp(value) && Object.keys(value).length === 0
+  return isEmptyObject || (!isImpliedValue && !isValueObject && !value.length)
 }
 
 export const returnNullValue = () => null
